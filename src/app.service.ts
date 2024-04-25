e450bfd9a5a0ac08ca9e52255e562ed463c7e78c
Daniel Carlos (Bugaboo) <daniel.carlos@bugaboostudio.com>
2024-04-25 15:33:20 -0300

    enable cors

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Daniel Carlos. Hello World!';
+  getHello(): any {
+    return { name: 'Daniel Carlos. Hello World!' };
   }
 
   getCarai(): string {
