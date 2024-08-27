1ac0f411019833a29e82b319f7caedb6b6a0d4fb
Ddaniel Ccarlos <daniel.hammerdev@gmail.com>
2024-08-27 08:55:03 -0300

    install swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+
+
+  // OpenAPI: Swagger ==========================================
+  const config = new DocumentBuilder()
+  .setTitle('Racing Bet')
+  .setDescription('The craziest betting game')
+  .setVersion('1.0')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  //============================================================
+
+  
   await app.listen(3000);
 }
 bootstrap();
